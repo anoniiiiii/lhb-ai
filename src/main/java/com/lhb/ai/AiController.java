ffe0188d9f39a530df592068061564182d929e0d
Long Huynh Bao <anoniiiiiimous@gmail.com>
2019-05-15 10:19:06 +0700

    First Commit

---
+++ src/main/java/com/lhb/ai/AiController.java
@@ -0,0 +1,21 @@
+package com.lhb.ai;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class AiController {
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.GET, value = "/chat")
+    public void chat(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().write("OK! I Chatted");
+        response.setStatus(200);
+    }
+
+}
